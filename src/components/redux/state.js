56c58e5182f79f960b13b9b45100bfe800b9885b
Edgar <edgar.mkhitaryan989@gmail.com>
2021-02-03 00:17:53 +0400

    Collectin into store

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,75 +1,80 @@
-let rerenderEntireTree = () => {
-  console.log('something');
-};
 
-let state = {
-  profilePage: {
-    posts: [
-      { message: "Hi, how are you?", id: 1, likesCount: 11 },
-      { message: "It's my first post.", id: 2, likesCount: 12 },
-      { message: "GG's", id: 3, likesCount: 55 },
-      { message: "gege", id: 4, likesCount: 120 },
-    ],
 
-    newPostText: "It_kamasutra",
+// let State = {
+//   id: ID,
+//   message: newMessage
+// }
+
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { message: "Hi, how are you?", id: 1, likesCount: 11 },
+        { message: "It's my first post.", id: 2, likesCount: 12 },
+        { message: "GG's", id: 3, likesCount: 55 },
+        { message: "gege", id: 4, likesCount: 120 },
+      ],
+  
+      newPostText: "It_kamasutra",
+    },
+  
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: '"HI, how are you?"' },
+        { id: 2, message: "I'm fine" },
+        { id: 3, message: "Yo" }
+      ],
+      dialogsData: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Valera" },
+        { id: 3, name: "Andrey" },
+        { id: 4, name: "Sash" },
+        { id: 5, name: "Vika" },
+        { id: 6, name: "Yura" },
+      ]
+  
+  
+    },
+  },
+  _rerenderEntireTree() {
+    console.log('something');
+  },
+  getState() {
+    return this._state;
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 11,
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._rerenderEntireTree(this._state);
   },
 
-  dialogsPage: {
-    messagesData: [
-      { id: 1, message: '"HI, how are you?"' },
-      { id: 2, message: "I'm fine" },
-      { id: 3, message: "Yo" }
-    ],
-    dialogsData: [
-      { id: 1, name: "Dimych" },
-      { id: 2, name: "Valera" },
-      { id: 3, name: "Andrey" },
-      { id: 4, name: "Sash" },
-      { id: 5, name: "Vika" },
-      { id: 6, name: "Yura" },
-    ]
-
-
+  updateNewPost(text) {
+    this._state.profilePage.newPostText = text;
+    this._rerenderEntireTree(this._state);
   },
-};
+  addNewMessage(text) {
+    let newMessage = {
+      id: 4,
+      message: text,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._rerenderEntireTree(this._state);
+  
+  },
+  updateMessage(text) {
+    this._state.dialogsPage.messagesData.message = text;
+    this._rerenderEntireTree(this._state);
+  },
+  subscriber(observer) {
+    this._rerenderEntireTree = observer;
+  }
+}
 
 
-
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 11,
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export let updateNewPost = (text) => {
-  state.profilePage.newPostText = text;
-  rerenderEntireTree(state);
-};
-
-export let addNewMessage = (text) => {
-  let newMessage = {
-    id: 4,
-    message: text,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  rerenderEntireTree(state);
-
-};
-
-export let updateMessage = (text) => {
-  state.dialogsPage.messagesData.message = text;
-  rerenderEntireTree(state);
-
-};
-
-export const subscriber = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { addPost, updateNewPost, updateMessage, addNewMessage, subscriber } from './components/redux/state';
 import * as serviceWorker from "./serviceWorker";
-import state from "./components/redux/state";
+import store from "./components/redux/state";
 
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(<App state={state} addPost={addPost} updateNewPost={updateNewPost} addNewMessage={addNewMessage} updateMessage={updateMessage} />, document.getElementById("root"));
+let rerenderEntireTree = () => {
+    ReactDOM.render(<App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPost={store.updateNewPost.bind(store)} addNewMessage={store.addNewMessage.bind(store)} updateMessage={store.updateMessage.bind(store)} />, document.getElementById("root"));
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree();
 
-subscriber(rerenderEntireTree);
+store.subscriber(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
